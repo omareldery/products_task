701b9600691328094d8e0b106c36af66855ac9bb
Omar Khaled <omark4367@gmail.com>
2024-07-14 01:50:06 +0300

    - add recycler_view.dart file

---
+++ lib/features/recycler/presentation_layer/views/recycler_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecyclerView extends StatelessWidget {
+  const RecyclerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
