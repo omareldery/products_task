bbcbb7f99229d13ad7cb9c2e102d690888e26d5e
Omar Khaled <omark4367@gmail.com>
2024-07-14 02:45:10 +0300

    - init recycler_view inside main - add appbar image to assets and pubspec.yaml

--- lib/features/recycler/presentation_layer/views/recycler_view.dart
+++ lib/features/recycler/presentation_layer/views/recycler_view.dart
@@ -5,6 +5,57 @@ class RecyclerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Image.asset(
+              'assets/images/route_image.png',
+              height: 80,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'What do you search for?',
+                      prefixIcon: Icon(Icons.search,color: Color(0xFF102F6B),),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(26)),
+
+                      ),
+                    focusedBorder:  OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF102F6B)),
+                      borderRadius: BorderRadius.all(Radius.circular(26)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF102F6B)),
+                      borderRadius: BorderRadius.all(Radius.circular(26)),
+
+                    )
+                  ),
+
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.shopping_cart_outlined,
+                color: Color(0xFF102F6B),
+              ),
+            )
+          ],
+        )
+      ],
+    ));
   }
 }
