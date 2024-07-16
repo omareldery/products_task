541e54256484a9b88181b2c1f5e8c6fcc9d5796a
Omar Khaled <omark4367@gmail.com>
2024-07-16 19:40:27 +0300

    - finished product_card.dart widget - added all API's files in networking like models and errors handler etc. - added favorite image in assets file - finished Product Card - finished Recycler Body - finished CustomGridView

--- lib/features/recycler/presentation_layer/views/recycler_view.dart
+++ lib/features/recycler/presentation_layer/views/recycler_view.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:products_task/core/helper/colors_manager.dart';
-import 'package:products_task/features/recycler/presentation_layer/widgets/product_card.dart';
+import 'package:products_task/features/recycler/data_layer/repos/product_repo.dart';
+import 'package:products_task/features/recycler/presentation_layer/manager/product_cubit.dart';
+import 'package:products_task/features/recycler/presentation_layer/widgets/recycler_body.dart';
 
 class RecyclerView extends StatelessWidget {
   const RecyclerView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-            ),
-            child: Image.asset(
-              'assets/images/route_logo.png',
-            ),
-          ),
-          Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: 'What do you search for?',
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: ColorsManager.kPrimaryColor,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(26)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: ColorsManager.kPrimaryColor),
-                          borderRadius: BorderRadius.all(Radius.circular(26)),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: ColorsManager.kPrimaryColor),
-                          borderRadius: BorderRadius.all(Radius.circular(26)),
-                        )),
-                  ),
+              Padding(
+                padding:  EdgeInsets.symmetric(horizontal: 16.w),
+                child: Image.asset(
+                  'assets/images/route_logo.png',
                 ),
               ),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.shopping_cart_outlined,
-                    color: ColorsManager.kPrimaryColor,
-                  ))
+              SizedBox(height: 16.h,),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 16.w),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'What do you search for?',
+                            prefixIcon: Icon(Icons.search,color: ColorsManager.kPrimaryColor,),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(26)),
+          
+                            ),
+                            focusedBorder:  OutlineInputBorder(
+                              borderSide: BorderSide(color: ColorsManager.kPrimaryColor),
+                              borderRadius: BorderRadius.all(Radius.circular(26)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: ColorsManager.kPrimaryColor),
+                              borderRadius: BorderRadius.all(Radius.circular(26)),
+          
+                            )
+                        ),
+          
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.shopping_cart_outlined,
+                      color: ColorsManager.kPrimaryColor,
+                    ),
+                  )
+                ],
+              ),
+              Expanded(
+                child: BlocProvider(
+                      create: (context) => ProductCubit(ProductRepository())..fetchProducts(),
+                      child: Padding(
+                        padding:  EdgeInsets.only(top: 16.h),
+                        child: const RecyclerBody(),
+                      ),
+                    ),
+              ),
             ],
           ),
-          const ProductCard(),
-        ],
-      ),
-    ));
+        ));
   }
 }
